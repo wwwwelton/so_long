665724e0cef165c1fb8a5a920407c3a3ea69d9f4
wleite <wleite@student.42sp.org.br>
2021-09-10 17:38:52 -0300

    test: print pressed key

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 21:21:11 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/10 15:24:10 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/10 15:43:31 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,12 @@ typedef struct s_game
 	int		win_height;
 }	t_game;
 
+typedef struct s_param{
+	int		x;
+	int		y;
+	char	str[3];
+}	t_param;
+
 # ifndef OPEN_MAX
 #  define OPEN_MAX 256
 # endif
@@ -43,6 +49,19 @@ typedef struct s_game
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
 # endif
+
+# define X_EVENT_KEY_PRESS 2
+# define X_EVENT_KEY_release 3
+# define X_EVENT_KEY_EXIT 17
+
+# define KEY_ESC 53
+# define KEY_Q 12
+# define KEY_W 13
+# define KEY_E 14
+# define KEY_R 15
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
 
 # define IMG_SPACE "assets/images/env_1/0.xpm"
 # define IMG_WALL "assets/images/env_1/1.xpm"
--- sources/so_long/so_long.c
+++ sources/so_long/so_long.c
@@ -6,15 +6,37 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 21:21:08 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/10 15:22:18 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/10 16:57:09 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
+int	key_press(int keycode, t_param *param)
+{
+	if (keycode == KEY_W)
+		param->x++;
+	else if (keycode == KEY_S)
+		param->x--;
+	else if (keycode == KEY_ESC)
+		exit(0);
+	printf("x: %d\n", keycode);
+	return (0);
+}
+
+void	param_init(t_param *param)
+{
+	param->x = 0;
+	param->y = 0;
+	param->str[0] = 'a';
+	param->str[1] = 'b';
+	param->str[2] = '\0';
+}
+
 int	main(int argc, char **argv)
 {
 	t_game	game;
+	t_param	param;
 
 	if (argc == 2)
 	{
@@ -24,6 +46,8 @@ int	main(int argc, char **argv)
 			game_init(&game);
 			img_init(&game);
 			map_init(&game);
+			param_init(&param);
+			mlx_key_hook(game.win, &key_press, &param);
 			mlx_loop(game.mlx);
 		}
 		else
