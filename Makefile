257fa35198c148939dc3c01cead1194b74077260
wleite <wleite@student.42sp.org.br>
2021-09-10 01:53:41 -0300

    feat: added GNL

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ LIBFT			=	$(LIBFT_PATH)/libft.a
 MINILIBX_PATH	=	$(SOURCES_DIR)/minilibx
 MINILIBX		=	$(MINILIBX_PATH)/libmlx.a
 
-SOURCES_FILES	=	so_long.c
+SOURCES_FILES	=	so_long.c map_gen.c get_next_line.c map_check.c
 
 SOURCES_DIR		=	sources
 
@@ -19,7 +19,7 @@ NAME			=	so_long
 CC				=	gcc
 RM				=	rm -f
 
-CFLAGS			=	-Wall -Wextra -Werror
+CFLAGS			=	-Wall -Wextra -Werror -g3 -fsanitize=address
 MLXFLAGS		=	-L. -lXext -L. -lX11
 
 .c.o:
@@ -28,7 +28,7 @@ MLXFLAGS		=	-L. -lXext -L. -lX11
 all:			$(NAME)
 
 $(NAME):		$(LIBFT) $(MINILIBX) $(OBJECTS)
-				$(CC) $(CFLAGS) $(OBJECTS) $(MINILIBX) $(MLXFLAGS) -I $(INCLUDES) -o $(NAME)
+				$(CC) $(CFLAGS) $(OBJECTS) $(LIBFT) $(MINILIBX) $(MLXFLAGS) -I $(INCLUDES) -o $(NAME)
 
 $(LIBFT):
 				$(MAKE) -C $(LIBFT_PATH)
--- includes/so_long.h
+++ includes/so_long.h
@@ -6,21 +6,37 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 21:21:11 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/09 14:59:06 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/10 01:52:13 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include <stdio.h>
-#include "../sources/minilibx/mlx.h"
-#include "../sources/libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include "../sources/libft/libft.h"
+# include "../sources/minilibx/mlx.h"
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 256
+# endif
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
 # define WIN_WIDTH 1000
 # define WIN_HEIGHT 1000
 
 # define IMG_WIDTH 1000
 # define IMG_HEIGHT 1000
+
+char	**map_gen(char *path_to_file);
+char	*get_next_line(int fd);
+
+int		map_check(char **map);
 
 #endif
