daa226182fa6359fc78de20d3a60a37090c4bb58
wleite <wleite@student.42sp.org.br>
2021-09-10 01:12:42 -0300

    refactor: remove GNL from libft

--- sources/libft/ft_uitoa_base.c
+++ sources/libft/ft_uitoa_base.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 17:59:04 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/08 17:11:21 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/08 20:48:52 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ static void	strrev(char *str)
 
 	start = 0;
 	end = ft_strlen(str) - 1;
-	if (str[start] == '-')
-		start++;
 	while (str[start] && start < end)
 	{
 		temp = str[start];
@@ -32,19 +30,14 @@ static void	strrev(char *str)
 	}
 }
 
-static size_t	ft_count_digits(size_t n)
+static size_t	ft_count_digits(size_t n, size_t base_len)
 {
 	size_t	i;
 
 	i = 0;
-	if (n < 0)
-	{
-		n = -n;
-		i++;
-	}
 	while (n)
 	{
-		n = n / 10;
+		n = n / base_len;
 		i++;
 	}
 	return (i);
@@ -58,16 +51,11 @@ char	*ft_uitoa_base(size_t n, const char *base)
 
 	if (n == 0)
 		return (ft_strdup("0"));
-	str = (char *)malloc(sizeof(char) * (ft_count_digits(n) + 1));
+	base_len = ft_strlen(base);
+	str = (char *)malloc(sizeof(char) * (ft_count_digits(n, base_len) + 1));
 	if (!str)
 		return (NULL);
-	base_len = ft_strlen(base);
 	i = 0;
-	if (n < 0)
-	{
-		n = -n;
-		str[i++] = '-';
-	}
 	while (n)
 	{
 		str[i++] = base[n % base_len];
