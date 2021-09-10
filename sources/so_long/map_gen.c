353b763359e0f7d1d1cbe8e9e4a5f30cbdeb0a43
wleite <wleite@student.42sp.org.br>
2021-09-10 01:54:13 -0300

    feat: added map generator from map file

---
+++ sources/so_long/map_gen.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_gen.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/10 00:43:20 by wleite            #+#    #+#             */
+/*   Updated: 2021/09/10 01:48:20 by wleite           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/so_long.h"
+
+char	**map_gen(char *path_to_file)
+{
+	char	**result;
+	char	*holder;
+	char	*old_address;
+	char	*map;
+	int		fd;
+
+	if (!path_to_file)
+		return (NULL);
+	fd = open(path_to_file, O_RDONLY);
+	map = ft_strdup("");
+	while (1)
+	{
+		holder = get_next_line(fd);
+		if (!holder)
+			break ;
+		old_address = map;
+		map = ft_strjoin(old_address, holder);
+		ft_free_ptr((void *)&old_address);
+		ft_free_ptr((void *)&holder);
+	}
+	result = ft_split(map, '\n');
+	ft_free_ptr((void *)&map);
+	return (result);
+}
--- sources/so_long/so_long.c
+++ sources/so_long/so_long.c
@@ -6,28 +6,66 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 21:21:08 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/09 19:01:27 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/10 01:49:39 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
-int main()
+int main(int argc, char **argv)
 {
-		void *mlx;
-		void *win;
-		void *img;
+	char	**map;
 
-		int		img_width;
-		int 	img_height;
-
-		mlx = mlx_init();
-		win = mlx_new_window(mlx, 500, 500, "my_mlx");
-		img = mlx_xpm_file_to_image(mlx, "../../assets/images/RPG-Nature-Tileset-Autumn.xpm", &img_width, &img_height);
-		mlx_put_image_to_window(mlx, win, img, 50, 50);
-		mlx_loop(mlx);
-		return (0);
+	if (argc == 2)
+	{
+		map = map_gen(argv[1]);
+		if (map_check(map))
+			map_draw(map);
+		else
+			printf("Map error!");
+	}
+	else
+		printf("No map specified.");
+	return (0);
 }
+
+// int main()
+// {
+// 		void *mlx;
+// 		void *win;
+// 		void *img;
+// 		void *img2;
+
+// 		int	i;
+
+// 		int		img_width;
+// 		int 	img_height;
+
+// 		mlx = mlx_init();
+// 		win = mlx_new_window(mlx, 300, 300, "my_mlx");
+// 		img = mlx_xpm_file_to_image(mlx, "assets/images/env_1/0.xpm", &img_width, &img_height);
+// 		img2 = mlx_xpm_file_to_image(mlx, "assets/images/env_1/C.xpm", &img_width, &img_height);
+// 		mlx_put_image_to_window(mlx, win, img, 0, 0);
+
+// 		i = 0;
+// 		while(i < 640)
+// 		{
+// 			mlx_put_image_to_window(mlx, win, img2, i, 0);
+// 			i += 32;
+// 		}
+
+// 		i = 0;
+// 		while(i < 640)
+// 		{
+// 			mlx_put_image_to_window(mlx, win, img2, 0, i);
+// 			i += 32;
+// 		}
+
+// 		mlx_loop(mlx);
+// 		return (0);
+// }
 
 //gcc main.c libmlx.a -L. -lXext -L. -lX11 && ./a.out
 //img.data[count_h * IMG_WIDTH + count_w] = 0x12154;
+
+
