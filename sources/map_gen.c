46c63fadd35d586c500a0084a55540389e6820bf
wleite <wleite@student.42sp.org.br>
2021-09-12 08:51:15 -0300

    fix: check if is invalid file descriptor

--- sources/map_gen.c
+++ sources/map_gen.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 00:43:20 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/11 03:58:03 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/12 08:48:54 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ char	**map_gen(char *path_to_file)
 	if (!path_to_file)
 		return (NULL);
 	fd = open(path_to_file, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
 	map = ft_strdup("");
 	while (1)
 	{
@@ -36,5 +38,6 @@ char	**map_gen(char *path_to_file)
 	}
 	result = ft_split(map, '\n');
 	ft_free_ptr((void *)&map);
+	close(fd);
 	return (result);
 }
